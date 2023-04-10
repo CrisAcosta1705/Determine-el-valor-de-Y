d78f71cb6299dd82cdb28325594705e66113fd55
CrisAcosta1705 <128009007+CrisAcosta1705@users.noreply.github.com>
2023-04-09 22:48:37 -0600

    Add files via upload

---
+++ CalcularY.java
@@ -0,0 +1,15 @@
+package Ejercicio;
+import java.util.Scanner;
+public class CalcularY {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner tc = new Scanner(System.in);
+		
+		double X = 2;
+		double C = 2.5;
+		double Y = X * C - 2;
+		System.out.println("El valor de Y es: " + Y);
+	}
+
+}
